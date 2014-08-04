3d48ef4c6672e96a90fb8cf5f1c3bcba7bb496c3
RouxRC <b.ooghe@gmail.com>
2014-08-04 05:13:26 +0200

    add lock

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,14 @@
 
 cd $(echo $0 | sed 's#/[^/]*$##')/..
 
-source $(which virtualenvwrapper.sh)
+if test -f lock.pid; then
+  exit 1
+fi
+echo $$ > lock.pid
+
+source /usr/local/bin/virtualenvwrapper.sh
 workon papToMails
 python papToMails.py
 deactivate
+
+rm -f lock.pid
