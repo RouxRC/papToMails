e421e69ba38ca6fccf074867d8f1f56c396c8c02
RouxRC <b.ooghe@gmail.com>
2017-01-26 18:16:22 +0100

    handle new SeLoger server securities

--- papToMails.py
+++ papToMails.py
@@ -5,6 +5,8 @@
 # No liability blah blah use at your own risk, etc
 
 import lxml.html as html
+from cStringIO import StringIO
+from gzip import GzipFile
 import re, time, sys
 import urllib2
 import smtplib
@@ -72,18 +74,29 @@ else:
         except: pass
         sendMail("PAP", text, text, url)
 
+def gunzip(s):
+    return GzipFile('', 'r', 0, StringIO(s)).read()
+
 # SELOGER.COM
 cururl = seloger_url
+opener = urllib2.build_opener()
+opener.addheaders = [
+    ('Pragma', 'no-cache'),
+    ('DNT', '1'),
+    ('Accept-Encoding', 'gzip, deflate, sdch'),
+    ('Accept-Language', 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4'),
+    ('Upgrade-Insecure-Requests', '1'),
+    ('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'),
+    ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
+    ('Cache-Control', 'no-cache'),
+    ('Cookie', 'mailD5=false; ASP.NET_SessionId=eb3ey2wgzcroacfntqwidv2x; __uzma=ma1d4c7379-bc06-46f3-a7de-a1f1ed92b5657477; __uzmb=1485449424; FirstVisitSeLoger=26/01/2017 16:50:24; HistoRecherches=GAEAAHicTcxBasMwEIXhq8ymykYl0thSSEDtFXqDINsTGIgVI8mFgOiheqocIyIi4MXwf28zunzzlLNDUXNfaGAKTsujGNkdjNJKy1ewpWvpW0yLFTmy48CZ/VUEn9f4/oMiDOeFaaTkOtlLA5ThAweR1njxI80cnFXll0KmBNPOL4uPmea6E9xWmAiut0s1dLIelPIFVsH8X7Cg3Su9R6UPoO3JqBP25cdHTqApSmjEx0xvdxv3G5uNbXX51H9PFhNXsg==; dtCookie=|U2VMb2dlcnww; vistId=bae02304-6c08-4225-bc59-01a04c829dbf; mailD5=false; __uzmc=921261961210; __uzmd=1485451735; SearchAnnDep=75; accept_cookies=true; __utma=208557584.2022651579.1485445891.1485445891.1485445891.1; __utmb=208557584.4.10.1485445891; __utmc=208557584; __utmz=208557584.1485445891.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'),
+    ('Connection', 'keep-alive')
+]
 while cururl:
-    doc = html.fromstring(urllib2.urlopen(cururl).read().decode("utf8"))
-    nexturl = doc.find_class("pagination_next")
-    if nexturl and nexturl[0].xpath("@href"):
-        cururl = "http://www.seloger.com/"+nexturl[0].xpath("@href")[0]
-    else:
-        cururl = ""
+    doc = html.fromstring(gunzip(opener.open(cururl).read()).decode("utf8"))
     annonces = doc.find_class("listing")
     for annonce in annonces:
-        url = annonce.xpath("div/div/div/a[@class='amount']/@href")[0]
+        url = annonce.xpath("div/div/div/a/@href")[0]
         url = url[:url.find('?')]
         if url in lastAnnonces:
             continue
@@ -91,7 +104,10 @@ while cururl:
             print >> sys.stderr, url
         else:
             lastAnnonces[url] = 1
-        taille = annonce.xpath("div/div/ul[@class='property_list']/li")[-1].text_content().encode('utf8')
+        try:
+            taille = annonce.xpath("div/div/div/ul[@class='property_list']/li")[-1].text_content().encode('utf8')
+        except:
+            taille = annonce.xpath("div/div/ul[@class='property_list']/li")[-1].text_content().encode('utf8')
         tel = annonce.xpath("div/div/div[@data-phone]/@data-phone")[0]
         title = annonce.xpath("div/div/h2")[0].text_content().encode("utf-8").replace("Appartement ", "")
         prix = annonce.xpath("div/div/div/a[@class='amount']")[0].text_content()
